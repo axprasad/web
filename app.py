4f1dbdab9ec1ab5a48ea4ad8c1d0aad118a2ee4d
Anand Prasad <axprasad@oradev.oraclecorp.com>
2018-06-28 21:42:17 -0700

    1.0

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
-from redis  import Redis
+from redis import Redis
 import os
 
 app = Flask(__name__)
 redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'),port=6379)
 
-@app.route('/')
+@app.route("/")
 def hello():
     redis.incr('hits')
     return 'Hello Cotnainer World !!!.  I have been seen %s times.\n'
 
+
+@app.route("/Web")
+def Web():
+    return 'My Web'
+
+    
 if __name__ == "__main__":
-    app.run (host = "0.0.0.0", port = 5000, debug = true)
+    app.run(host='0.0.0.0', port=5000, debug=True)
